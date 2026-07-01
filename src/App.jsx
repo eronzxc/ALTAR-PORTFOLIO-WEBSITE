396ca426711ce405ec2b33a21648874935683e11
eronzxc <aaronaltar@gmail.com>
2026-07-01 13:42:55 +0800

    added import function for certificate part

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
+import Certificates from './components/Certificates';
 
 function App() {
   return (
